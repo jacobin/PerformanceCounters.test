49358953722c96c7b2143aa2232ee66b5f818d6e
HarlyJacobin <harly.pe@gmail.com>
2020-09-11 10:05:24 +0800

    https://stackoverflow.com/questions/3896685/simplest-possible-performance-counter-example

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//https://stackoverflow.com/questions/3896685/simplest-possible-performance-counter-exampleusing System;
+using System.Linq;
+using System.Diagnostics;
+using System;
+
+namespace TestStopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            var processorCategory = PerformanceCounterCategory.GetCategories()
+                .FirstOrDefault(cat => cat.CategoryName == "Processor");
+            var countersInCategory = processorCategory.GetCounters("_Total");
+
+            DisplayCounter(countersInCategory.First(cnt => cnt.CounterName == "% Processor Time"));
+        }
+
+        private static void DisplayCounter(PerformanceCounter performanceCounter)
+        {
+            while (!Console.KeyAvailable)
+            {
+                Console.WriteLine("{0}\t{1} = {2}",
+                    performanceCounter.CategoryName, performanceCounter.CounterName, performanceCounter.NextValue());
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
